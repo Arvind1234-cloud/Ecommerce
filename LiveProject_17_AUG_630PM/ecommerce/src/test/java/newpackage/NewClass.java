317b928eaf79b8e2f43b90bb5d267c446d81a49e
Mahesh <seleniumbymahesh@gmail.com>
2018-11-15 19:18:38 +0530

    Revert "extra changes in new class"
    This reverts commit 3b2cbefa4c6b962f02e016aa53aa1cb20847a3ec.

--- LiveProject_17_AUG_630PM/ecommerce/src/test/java/newpackage/NewClass.java
+++ LiveProject_17_AUG_630PM/ecommerce/src/test/java/newpackage/NewClass.java
@@ -4,7 +4,6 @@ public class NewClass {
 
 	public static void main(String[] args) {
 		System.out.println("Hi i am updated one packa nd one class");
-		System.out.println("Extra modifications");
 
 	}
 

3b2cbefa4c6b962f02e016aa53aa1cb20847a3ec
Mahesh <seleniumbymahesh@gmail.com>
2018-11-15 19:14:39 +0530

    extra changes in new class

--- LiveProject_17_AUG_630PM/ecommerce/src/test/java/newpackage/NewClass.java
+++ LiveProject_17_AUG_630PM/ecommerce/src/test/java/newpackage/NewClass.java
@@ -4,6 +4,7 @@ public class NewClass {
 
 	public static void main(String[] args) {
 		System.out.println("Hi i am updated one packa nd one class");
+		System.out.println("Extra modifications");
 
 	}
 
